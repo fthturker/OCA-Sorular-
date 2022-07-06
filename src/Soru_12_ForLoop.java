4565d643d7486a401cdffa30a78ea694cb50026a
Fatih Turker <fthturker80@gmail.com>
2022-07-06 10:20:04 +0300

    06.07.2022

--- src/Soru_12_ForLoop.java
+++ src/Soru_12_ForLoop.java
@@ -12,19 +12,39 @@ public class Soru_12_ForLoop {
         // islemi baslatan ve bitiren degisken ayni ise
 
         // 1'den 100 e kadar (100 dahil) olan tamsayilari toplanyin
-        int toplam=0;
-        for (int i = 1; i <100 ; i++) {
-            toplam+=i;
-        }
-        System.out.println(i+);
+        int toplam = 0;
+        for (int i = 1; i <= 100; i++) {
+            toplam += i;
 
+        }
+        System.out.print(toplam);
+        System.out.println("");
+
+        // verilen iki sayinin ilkinden baslayip ikinciye kadar
+        // 3'er 3'er yazdirin (ikinci sayi sartlari saglamiyorsa yazdirilmayabilir)
+        int bas = 20;
+        int bitis = 61;
+        for (int i = bas; i < bitis; i += 3) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        // verilen sayidan baslayarak geriye 1'e dogru
+        // 3 ile bolunebilen sayilari yazdirin
+        int input = 100;
+        for (int i = input; i >= 1; i--) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+/*
                 for (int i = 0; i < 10; ) {
 
             i = i++; // post increment once atama sonra artırma i hep 0 olur
 
             System.out.println(" Hello World");
         }
-
+*/
 
     }
 }
