5c9d2d42c07a682bf1e2b3bef5bcf9ff262cf608
Fatih Turker <fthturker80@gmail.com>
2022-07-06 10:05:18 +0300

    06-07-2022

--- src/Soru_12_ForLoop.java
+++ src/Soru_12_ForLoop.java
@@ -1,12 +1,31 @@
 public class Soru_12_ForLoop {
     public static void main(String[] args) {
+        // FOR LOOP belirli sayida veya belirli sartlar saglanincaya kadar tekrar edilmesi gereken
+        // bir islemi verimli bir sekilde yazmaniza olanak taniyan kod bloklaridir
+        // verilen bir String`i tersine cevirin
+        // verilen bir sayidan 1 e kadar tum sayilari toplayin
+        // verilen bir tamsayinin rakamlar toplamini bulun
+        // kullanicinin verdigi sayilari, toplam 500 oluncaya kadar toplayin
+        // kullanicinin verdigi sayilari kullanici tamam diyene kadar toplayin
+        // avantaj yapan durumlar islemde
+        // islemi kac kere yapacaginiz belli ise
+        // islemi baslatan ve bitiren degisken ayni ise
 
-        for (int i = 0; i < 10;) {
+        // 1'den 100 e kadar (100 dahil) olan tamsayilari toplanyin
+        int toplam=0;
+        for (int i = 1; i <100 ; i++) {
+            toplam+=i;
+        }
+        System.out.println(i+);
 
-            i =i++; // post increment once atama sonra artırma i hep 0 olur
+                for (int i = 0; i < 10; ) {
+
+            i = i++; // post increment once atama sonra artırma i hep 0 olur
 
             System.out.println(" Hello World");
         }
+
+
     }
 }
 /*
